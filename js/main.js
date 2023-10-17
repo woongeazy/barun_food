d2c09c8ada83101cb619e214ae018a2ae2dc4747
hanchaewoong <hco2995@naver.com>
2023-10-18 02:07:41 +0900

    캐러셀 진행중

--- js/main.js
+++ js/main.js
@@ -5,4 +5,15 @@ $(function(){
     autoplay: true,
     autoplaySpeed: 2000,
   });
+
+  $("#hero").on("mouseover", function(e){
+    $(".left_arr").css("background", "rgba(255, 255, 255, 0.5)")
+    $(".right_arr").css("background", "rgba(255, 255, 255, 0.5)")
+    console.log("over");
+  });
+  $("#hero").on("mouseout", function(e){
+    console.log("asdf");
+    $(".left_arr").css("background", "none")
+    $(".right_arr").css("background", "none")
+  })
 })
